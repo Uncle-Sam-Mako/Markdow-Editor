ba71bdbca35ad41968ae7e77c5f32436c7210a19
Uncle-Sam-Mako <bwsamuel6@gmail.com>
2021-08-23 21:04:21 +0200

    Update App.js : rendering HTML with marked

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
-import {sampleText} from './sampleText'
+import {sampleText} from './sampleText';
+import marked from 'marked';
 
 class App extends Component {
   state = {
     value : sampleText
+  }
+  renderText = text => {
+    const __html = marked(text, {sanitize:true});
+    return { __html }
   }
   handleChange = e => {
     this.setState({value : e.target.value})
@@ -18,7 +23,9 @@ class App extends Component {
           <div className="col-sm-6">
             <textarea onChange={this.handleChange} value={this.state.value} className="form-control" name="markdown-side" id="markdown-side" cols="30" rows="35"></textarea>
           </div>
-          <div className="col-sm-6">{sampleText}</div>
+          <div className="col-sm-6">
+            <div dangerouslySetInnerHTML={this.renderText(this.state.value)}></div>
+          </div>
         </div>
       </div>
     );
